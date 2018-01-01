8b98139f5562a6b5a3e216e640ce315d2ab13455
Alisahhh <923431911@qq.com>
2018-01-01 23:43:40 +0800

    I can only open one page but I don't know why

--- server.go
+++ server.go
@@ -4,9 +4,9 @@ import (
 	"fmt"
 	"net"
 	"os"
-	"time"
 	"log"
 	"strconv"
+	"encoding/binary"
 )
 
 type IOpair struct{
@@ -21,292 +21,127 @@ func checkError(err error) {
 	}
 }
 
+
+func BytesToInt32(buf []byte) int {
+	return int(binary.BigEndian.Uint32(buf))
+}
+
 func main() {
-	//service := ":1200"
-	//tcpAddr, err := net.ResolveTCPAddr("tcp", service)
-	//checkError(err)
 	l, err := net.Listen("tcp", ":1200")
 	checkError(err)
-	//conns :=make(map[uint32]IOpair)
 	for{
 		conn,err:=l.Accept()
+
 		if err != nil {
 			log.Panic(err)
+			continue
 		}
-
 		go handleClientRequest(conn)
 	}
 }
 
 func handleClientRequest(conn net.Conn){
-	var hsm auth
-	buf:=make([]byte,1)
-	_,err:=conn.Read(buf)
-	if err != nil {
-		fmt.Printf("Network Error.Terminating..\n")
-		return
-	}
-	if buf[0] != 0x05 {
-		fmt.Printf("Protocol implementation error.Terminating...\n")
-		return
-	}
-	_,err=conn.Read(buf)
-	if err != nil {
-		fmt.Printf("Network Error.Terminating..\n")
-		return
-	}
-	hsm.VER=0x05
-	hsm.NMETHODS=buf[0]
-	//buf=make([]byte,buf[0])
-	hsm.METHODS=make([]byte,buf[0])
-	_,err=conn.Read(hsm.METHODS)
-	if err != nil {
-		fmt.Printf("Network Error.Terminating..\n")
-		return
-	}
-	hsm.NMETHODS=0x01
-	//buf=make([]byte,buf[0])
-	hsm.METHODS[0]=0x00
-
-	var authRep authReply
-	authRep.VER = 0x05
-	authRep.METHOD=0x00
-	//tmpbuf:=make([]byte,2)
-	_,err=conn.Write([]byte{0x05, 0x00})
-	if err!=nil{
-		fmt.Printf("Network Error.Terminating..\n")
-		return
-	}
-	//auth success
-	for{
-		//cnt++
-		//fmt.Println(cnt)
-		if err!=nil{
-			continue
-		}
-		/*buf:=make([]byte,4)
-		var zgr getRequest
-		_,err=conn.Read(buf)
-		if err != nil {
-			fmt.Printf("Network Error %v.Terminating..\n", err.Error())
-			os.Exit(-1)
-		}
-		zgr.VER=buf[0]
-		zgr.CMD=buf[1]
-		zgr.RSV=buf[2]
-		zgr.ATYP=buf[3]
-		if zgr.ATYP==0x01{
-			zgr.ADDR=make([]byte,4)
-			_,err:=conn.Read(zgr.ADDR)
-			if err != nil {
-				fmt.Printf("Network Error %v.Terminating..\n", err.Error())
-				os.Exit(-1)
-			}
-			host = net.IPv4().String()
-		}else if zgr.ATYP==0x04{
-			zgr.ADDR=make([]byte,16)
-			_,err:=conn.Read(zgr.ADDR)
-			if err != nil {
-				fmt.Printf("Network Error %v.Terminating..\n", err.Error())
-				os.Exit(-1)
-			}
-		}else if zgr.ATYP==0x03{//change
-			tmpbuf=make([]byte,1)
-			_,err=conn.Read(tmpbuf)
-			if err != nil {
-				fmt.Printf("Network Error %v.Terminating..\n", err.Error())
-				os.Exit(-1)
-			}
-			buf=make([]byte,int(tmpbuf[0]))
-			_,err=conn.Read(buf)
-			if err != nil {
-				fmt.Printf("Network Error %v.Terminating..\n", err.Error())
-				os.Exit(-1)
-			}
-			zgr.ADDR=make([]byte,int(tmpbuf[0])+1)
-			zgr.ADDR[0]=tmpbuf[0]
-			for i:=0;i<int(tmpbuf[0]);i++{
-				zgr.ADDR=append(zgr.ADDR,buf[i])
-			}
-
-		}
-
-		zgr.PORT=make([]byte,2)
-		_,err=conn.Read(zgr.PORT)
-		if err != nil {
-			fmt.Printf("Network Error %v.Terminating..\n", err.Error())
-			os.Exit(-1)
-		}*/
-
-		/*tmpLen:=make([]byte,4)
-		_,err=conn.Read(tmpLen)
-		if err != nil {
-			fmt.Printf("Network Error %v.Terminating..\n", err.Error())
-			os.Exit(-1)
-		}
-		zgr.datalength=binary.LittleEndian.Uint32(tmpLen)
-
-		if zgr.datalength!=0{
-			zgr.data=make([]byte,zgr.datalength)
-			_,err:=conn.Read(zgr.data)
-			if err != nil {
-				fmt.Printf("Network Error %v.Terminating..\n", err.Error())
-				os.Exit(-1)
-			}
-		}*/
-		var b [1024]byte
-		n, err := conn.Read(b[:])
+		defer conn.Close()
+		var b []byte
+		b=make([]byte,4)
+		_,err := conn.Read(b)
+		n:=BytesToInt32(b)
+		//fmt.Println(n)
+		b=make([]byte,n)
+		_, err = conn.Read(b)
+		//b=b[:n]
+		fmt.Println("--------------52------------------")
+		fmt.Println(b)
 		if err != nil {
 			log.Println(err)
 			return
 		}
+
+		//DNS
 		var zgr getRequest
-		if b[0] == 0x05 { //只处理Socks5协议
-			//客户端回应：Socks服务端不需要验证方式
-			conn.Write([]byte{0x05, 0x00})
-			n, err = conn.Read(b[:])
-			//var host, port string
+		if b[0] == 0x05 {
+			b=b[:n]
 			switch b[3] {
 			case 0x01: //IP V4
-				zgr.ADDR= net.IPv4(b[4], b[5], b[6], b[7]).String()
-			case 0x03:                   //域名
+				zgr.ADDR = net.IPv4(b[4], b[5], b[6], b[7]).String()
+			case 0x03:                       //域名
 				zgr.ADDR = string(b[5: n-2]) //b[4]表示域名的长度
 			case 0x04: //IP V6
 				zgr.ADDR = net.IP{b[4], b[5], b[6], b[7], b[8], b[9], b[10], b[11], b[12], b[13], b[14], b[15], b[16], b[17], b[18], b[19]}.String()
 			}
 			zgr.PORT = strconv.Itoa(int(b[n-2])<<8 | int(b[n-1]))
-		}else{
+		} else {
 			fmt.Printf("Protocol implementation error.Terminating...\n")
 			return
 		}
-		go func (zgr getRequest){
-			//zgr.data= AESDecrypt(zgr.data)
-			//pair, isok := conns[addrxport2id(zgr.ADDR, zgr.PORT)]
-			//if !isok{
-			var tmpiop,pair IOpair
-			tmpiop.in = make(chan []byte, 5)
-			tmpiop.out = make(chan []byte, 5)
-			go func(){
-				//var finalTcpAddr net.TCPAddr
-				//finalTcpAddr.IP=zgr.ADDR
-				//finalTcpAddr.Port=int(binary.BigEndian.Uint16(zgr.PORT))
-
-				sconn, err := net.Dial("tcp", net.JoinHostPort(zgr.ADDR, zgr.PORT))
-				if err!=nil{
-					fmt.Printf("NetworkError.Cancelling...\n")
+		//mt.Println(zgr.ADDR)
+		//fmt.Println(zgr.PORT)
+		sconn, err := net.Dial("tcp", net.JoinHostPort(zgr.ADDR, zgr.PORT))
+		if err != nil {
+			//conn.Write([]byte{0x00})
+			log.Println(err)
+			return
+		}
+		defer sconn.Close()
+		fmt.Println(net.JoinHostPort(zgr.ADDR, zgr.PORT))
+		//sconn, err := net.Dial("tcp", "127.0.0.1:80")
+		if err != nil {
+			fmt.Printf("NetworkError.Cancelling...\n 81")
+			fmt.Println(err.Error())
+			//delete(conns, addrxport2id(zgr.ADDR, zgr.PORT))
+			return
+		}
+		//send to server
+		go func(){
+			for {
+				var b []byte
+				b = make([]byte, 4)
+				_, err := conn.Read(b)
+				n := BytesToInt32(b)
+				//fmt.Println(n)
+				b = make([]byte, n)
+				_, err = conn.Read(b)
+				//b=b[:n]
+				fmt.Println("--------------97------------------")
+				fmt.Println(b)
+				if err != nil {
+					log.Println(err)
+					return
+				}
+				_, err = sconn.Write(b)
+				if err != nil {
+					fmt.Printf("NetworkError.Cancelling...103\n")
 					fmt.Println(err.Error())
 					//delete(conns, addrxport2id(zgr.ADDR, zgr.PORT))
 					return
 				}
-				defer func(){
-					//delete(conns, addrxport2id(zgr.ADDR, zgr.PORT))
-					sconn.Close()
-				}()
-				/*go func(){
-					for{
-						tmpbuf=make([]byte,1024)
-						n,err:=sconn.Read(tmpbuf)
-						if err != nil {
-							fmt.Printf("Prox to Dest Conn error %v. Terminating..\n", err.Error())
-							close(tmpiop.out)
-							delete(conns, addrxport2id(zgr.ADDR, zgr.PORT))
-							sconn.Close()
-							return
-						}
-						tmpbuf=tmpbuf[:n]
-						tmpiop.out<-tmpbuf
-					}
-				}()*/
-				for{
-					select{
-					case data :=<-tmpiop.in:
-						sconn.Write(hsm.toByteArr())
-						tmpbuf:=make([]byte,2)
-						_,err=sconn.Read(tmpbuf)
-						if err!=nil||tmpbuf[0]!=0x05||tmpbuf[1]!=0x00{
-							fmt.Printf("Network Error %v.Terminating..\n", err.Error())
-							os.Exit(-1)
-						}
-						_,err:=sconn.Write(data)
-						if err!=nil{
-							fmt.Printf("Network Error.Retrying(In case http closure)..\n")
-							sconn, err := net.Dial("tcp", net.JoinHostPort(zgr.ADDR, zgr.PORT))
-							if err!=nil{
-								fmt.Printf("Network fail retry failed.Terminating..\n")
-								return
-							}
-							defer sconn.Close()
-							_,err=sconn.Write(data)
-							if err!=nil{
-								fmt.Printf("Network fail retry failed.Terminating..\n")
-								return
-							}
-						}
-						tmpbuf=make([]byte,1024)
-						n,err:=sconn.Read(tmpbuf)
-						if err != nil {
-							fmt.Printf("Prox to Dest Conn error %v. Terminating..\n", err.Error())
-							close(tmpiop.out)
-							//delete(conns, addrxport2id(zgr.ADDR, zgr.PORT))
-							sconn.Close()
-							return
-						}
-						tmpbuf=tmpbuf[:n]
-						tmpiop.out<-tmpbuf
-					case <-time.After(20 * time.Second):
-						fmt.Printf("Timed out.Terminating..\n")
-						return
-					}
-				}
-			}()
-			//conns[addrxport2id(zgr.ADDR, zgr.PORT)] = tmpiop
-			//pair=tmpiop
-			go func(){
-				for{
-					tmpbuff, ok := <-pair.out
-					if(!ok){
-						return
-						/*var zgrr getReply
-						zgrr.VER = zgr.VER
-						zgrr.REP = zgr.CMD
-						zgrr.PORT = zgr.PORT
-						zgrr.ATYP = zgr.ATYP*/
-
-						//zgrr.data = make([]byte, 1)
-						/*_, err := conn.Write(zgrr.toByteArr())
-						if err != nil {
-							fmt.Printf("Interserver side Conn error %v.Terminating..\n", err.Error())
-							conn.Close()
-							os.Exit(-1)
-						}
-						return*/
-					}
-
-					fmt.Printf("Received str:\n%v\n", string(tmpbuff))
-					//conn.Write(tmpbuff)
-					/*var zgrr getReply
-					zgrr.VER = zgr.VER
-					zgrr.REP = zgr.CMD
-					zgrr.PORT = zgr.PORT
-					zgrr.ATYP = zgr.ATYP
-					zgrr.datalength = uint32(len(tmpbuff))
-					zgrr.data=tmpbuff
-					//zgrr.data = AESEncrypt(tmpbuff)*/
-
-					go func() {
-						_, err := conn.Write(tmpbuff)
-						if err != nil {
-							fmt.Printf("Network error.Terminating..\n")
-							conn.Close()
-							os.Exit(-1)
-						}
-					}()
-
-				}
-			}()
-			//}
-			pair.in <- zgr.toByteArr()
-		}(zgr)
-	}
+			}
+		}()
+		//sent to client
+		for{
+			//fmt.Println("prepare to read")
+			buf:=make([]byte,10240)
+			n,err=sconn.Read(buf)
+			buf=buf[:n]
+			fmt.Println("--------------117------------------")
+			fmt.Println(buf)
+			//fmt.Println("Readok")
+			if err!=nil {
+				fmt.Printf("NetworkError.Cancelling...117\n")
+				fmt.Println(err.Error())
+				//delete(conns, addrxport2id(zgr.ADDR, zgr.PORT))
+				return
+			}
+			//buf=buf[:n]
+			//fmt.Println(buf)
+			n,err=conn.Write(buf)
+			if err!=nil {
+				fmt.Printf("NetworkError.Cancelling...126\n")
+				fmt.Println(err.Error())
+				//delete(conns, addrxport2id(zgr.ADDR, zgr.PORT))
+				return
+			}
+			//fmt.Println("Writeok")
+		}
 }
 
